0563d122c4cf6c12dad7172b270e1fd5105397c1
Rahulshahare <rahulshahare@gmail.com>
2021-05-15 22:08:40 +0530

    Conneted  to ONLINE_MONGODB database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server' );
 const gql = require('graphql-tag');
-
+const mongoose = require('mongodb');
+const {MONGODB} = require('./config');
 
 const typeDefs  = gql`
     type Query {
@@ -19,7 +20,12 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5000})
+
+mongoose.connect( MONGODB, { useNewUrlParser: true} )
+    .then(() => {
+        console.log('connected to MONGODB');
+        return server.listen({ port: 5000})
+    })
     .then(res =>{
         console.log('Server runing at'+ res.url)
     })
